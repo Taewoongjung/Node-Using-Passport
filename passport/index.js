a389fe1ce14b80f3c355c3781cff94a53e2de0d8
tae <aipooh8882@naver.com>
2021-01-30 00:43:48 +0900

    correct wrong

--- passport/index.js
+++ passport/index.js
@@ -11,7 +11,7 @@ module.exports = () => {
         User.findOne({
             where: { id }
         })
-    })
-    .then(user => done(null, user))
-    .catch(err => done(err));
+        .then(user => done(null, user))
+        .catch(err => done(err));
+    });
 };
